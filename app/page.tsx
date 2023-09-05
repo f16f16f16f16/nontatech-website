4c8587143e5d0dcfaffc17558abd858bfc7c3c4a
Nontaphat Pongpis <momo5101@hotmail.com>
2023-09-05 18:14:20 +0700

    Remove arrow

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Arrow from "@/component/arrow/index";
-
 export default function Home() {
   return (
     <div>
@@ -8,11 +6,6 @@ export default function Home() {
           Nontaphat Pongpis
         </h1>
         <p className="text-3xl text-gray-600">Software Engineer</p>
-        {/* <span className="relative flex h-3 w-3">
-          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-          <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
-        </span> */}
-        <Arrow />
       </div>
     </div>
   );
