0cd8572d8de7c5793ecfb30ed3d7a7f6e9a0a94e
Nontaphat Pongpis <momo5101@hotmail.com>
2023-09-05 18:13:06 +0700

    Change text style

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+import Arrow from "@/component/arrow/index";
+
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-3xl mb-4">Nontaphat Pongpis</h1>
-      <p className="text-lg">Software Engineer</p>
+    <div>
+      <div className="min-h-screen flex flex-col items-center justify-center">
+        <h1 className="text-5xl font-bold mb-6 text-center text-indigo-700">
+          Nontaphat Pongpis
+        </h1>
+        <p className="text-3xl text-gray-600">Software Engineer</p>
+        {/* <span className="relative flex h-3 w-3">
+          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+          <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+        </span> */}
+        <Arrow />
+      </div>
     </div>
   );
 }
